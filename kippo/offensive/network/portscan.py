8a13e08a3e290acc7beba10381cee2308340a902
jongreenall <jon2100@gmail.com>
2013-04-23 16:31:10 +0100

    Portscanning now supports banner grabbing. Default to -A nmap arg.

--- kippo/offensive/network/portscan.py
+++ kippo/offensive/network/portscan.py
@@ -24,14 +24,36 @@ class PortScan(object):
             print "!!! PortScan error. Did you request a scan type that requires root?"
             return
             
-        scanOpen = []
+        openTcpPorts = []
+        openUdpPorts = []
         try:
             for port in nm[self.clientip]['tcp'].keys():
-                scanOpen.append(port)
+                openTcpPorts.append(port)
         except KeyError:
             pass
             
-        resultsString = "Port scan results for %s, open TCP: %s." % (self.clientip, str(list(set(scanOpen))))
+        try:
+            for port in nm[self.clientip]['udp'].keys():
+                openUdpPorts.append(port)
+        except KeyError:
+            pass
+        
+        resultsString = ""
+        for port in openTcpPorts:
+            resultsString += "TCP Port open: %s\n" % (port)
+            
+            portInfo = nm[self.clientip]['tcp'][port]
+            for key in portInfo:
+                resultsString += "\t - %s: %s\n" % (key,  portInfo[key])
+                
+        for port in openUdpPorts:
+            resultsString += "UDP Port open: %s\n" % (port)
+            
+            portInfo = nm[self.clientip]['udp'][port]
+            for key in portInfo:
+                resultsString += "\t - %s: %s\n" % (key,  portInfo[key])
+            
+            
         print resultsString
         
         sendEmail('Port scan results ' + self.clientip,  resultsString)
